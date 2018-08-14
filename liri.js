80f889c146ce94ffc7240452e8b0b1c2def1613f
r.lim406 <r.lim406@hotmail.com>
2018-08-14 19:07:10 -0400

    corrected env file

--- liri.js
+++ liri.js
@@ -3,9 +3,9 @@ var input = process.argv;
 var command = input[2];
 var keys = require('./keys.js');
 var request = require('request');
-// var Spotify = require('node-spotify-api');
-// var spotify = new Spotify(keys.spotify);
-// var twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var twitter = require('twitter');
 var x = "";
 
 for (var i = 3; i < input.length; i++) {
@@ -43,22 +43,22 @@ switch (command) {
 };
 
 
-// function spotifySearch() {
-//   spotify.search({ type: 'track', query: song }, function (err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//       console.log(data);
-//     } else if (!err) {
-//       for (var i = 0; i < data.tracks.items.length; i++) {
-//         var song = data.tracks.items[i];
-//         console.log("Artist" + song.artist[0].name);
-//         console.log("Song: " + song.name);
-//         console.log("Preview URL: " + song.preview_url);
-//         console.log("Album: " + song.album.name);
-//       }
-//     }
-//   });
-// }
+function spotifySearch() {
+  spotify.search({ type: 'track', query: song }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+      console.log(data);
+    } else if (!err) {
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        var song = data.tracks.items[i];
+        console.log("Artist" + song.artist[0].name);
+        console.log("Song: " + song.name);
+        console.log("Preview URL: " + song.preview_url);
+        console.log("Album: " + song.album.name);
+      }
+    }
+  });
+}
 
 function omdbSearch(x) {
   var omdbURL = "http://www.omdbapi.com/?t=" + x + "&plot=short&tomatoes=true&apikey=trilogy";
